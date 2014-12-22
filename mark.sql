93483b19d1aeb76415f9111ff3cb77a500e82b74
Dodd2013 <291106637@qq.com>
2014-12-22 09:19:40 +0800

    12229:19

--- mark.sql
+++ mark.sql
@@ -31,12 +31,21 @@ Per_Remarks varchar(80)  null
  password varchar(50) not null,
  Emp_Id varchar(4) foreign key references EmployeeTB not null
  )
-
  insert into EmployeeTB values ('E001','Dodd','Dodd','man','1995-09-25','15066817901','291106637@qq.com','青大汇园二号',getdate())
  insert into usersTB values('Dodd','3d4f2bf07dc1be38b20cd6e46949a1071f9d0e3d','E001')
- insert into ModelTB values (1002,'EMP Management','100EMP Management','增删改查')
+ insert into ModelTB values (1001,'EMP Management','100EMP Management','增删改查')
+ insert into ModelTB values (1002,'User Management','100EMP Management','增删改查')
+ insert into ModelTB values (2001,'ItemDetail Management','100EMP Management','增删改查')
+ insert into PermissionTB values ('E001',1001,'---')
  insert into PermissionTB values ('E001',1002,'---')
 
 
  -------------------------------------------------------------------------------
- create table 
+ create table ItemDetailTB(
+ Item_Id int primary key identity(1,1) not null,
+ Item_NameCN varchar(50) not null,
+ Item_NameEN varchar(50) not null,
+ supplier varchar(50) null,
+ descriptions varchar(300) null,
+ price int not null
+ )
